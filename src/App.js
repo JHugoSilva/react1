2d1294f06af74754833be5d2f0aa0875d0756b54
JHugoSilva <hugosilvahs31@gmail.com>
2020-04-26 10:12:09 -0300

    Add Styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import style from 'styled-components'
+import styled from 'styled-components'
 
+const Site = styled.div`
+  width:400px;
+  height:400px;
+  background-color:#00FF00;
+`
+const Button = styled.button`
+  font-size:19px;
+  padding:10px 15px;
+  border:3px dashed #FF0000;
+  color:#FF0000;
+  background-color:#FFF;
+  margin:5px;
+  border-radius:5px;
+`
+const ButtonSmall = styled(Button)`
+  font-size:16px;
+  padding:5px 10px;
+`
 function App() {
   return <>
-
+    <Site>
+      <Button>Click</Button>
+      <ButtonSmall>Click</ButtonSmall>
+    </Site>
   </>
 }
 
